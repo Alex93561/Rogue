e3b1708bc1d310823d6da5055aa05b4fc45bc77f
AlexNK001 <alexandrnovickij92@gmail.com>
2023-11-30 19:11:34 +0300

    init

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] private float _speed = 1;
+
+    private Transform _transform;
+
+    private void Start()
+    {
+        _transform= transform;
+    }
+
+    private void Update()
+    {
+       _transform.position = Vector3.MoveTowards(_transform.position, _transform.position + Vector3.forward, _speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Signaling.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using UnityEngine;
+
+public class Signaling : MonoBehaviour
+{
+    [SerializeField] private AudioSource _audioSource;
+
+    private float _step = 0.05f;
+    private float _minVolume = 0;
+    private float _maxVolume = 1;
+    private WaitForSeconds _sleep = new WaitForSeconds(0.1f);
+    private Coroutine _changeVolume;
+
+    private void Start()
+    {
+        _audioSource.volume = _minVolume;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (_audioSource.isPlaying == false)
+        {
+            _audioSource.Play();
+        }
+
+        _changeVolume = StartCoroutine(ChangeVolume(_maxVolume));
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        _changeVolume = StartCoroutine(ChangeVolume(_minVolume));
+    }
+
+    private IEnumerator ChangeVolume(float volume)
+    {
+        if (_changeVolume != null)
+        {
+            StopCoroutine(_changeVolume);
+        }
+
+        while (_audioSource.volume != volume)
+        {
+            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, volume, _step);
+
+            yield return _sleep;
+        }
+
+        StopCoroutine(_changeVolume);
+
+        if (_audioSource.volume == _minVolume)
+        {
+            _audioSource.Stop();
+        }
+    }
+}
